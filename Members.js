ae26e0b63fc02ad63ddd38080445c83392a5c33c
jhaajay749 <jhaajay749@gmail.com>
2022-02-24 10:26:35 +0530

    express crash course

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: "Ajay",
+    email: "ajay@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Aman",
+    email: "aman@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Nayan",
+    email: "nayan@gmail.com",
+    status: "active",
+  },
+  {
+    id: 4,
+    name: "Neeraj",
+    email: "neeraj@gmail.com",
+    status: "inactive",
+  },
+];
+
+module.exports = members;
